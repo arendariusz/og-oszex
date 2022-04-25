dc4ee27072090b6ddb96137e367847af28206530
arendariusz <d.arendarski92@gmail.com>
2022-04-25 17:58:18 +0200

    Ogłoszex

---
+++ src/main/java/com/example/ogloszex/OgloszexApplication.java
@@ -0,0 +1,13 @@
+package com.example.ogloszex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgloszexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgloszexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ogloszex/OgloszexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ogloszex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgloszexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
